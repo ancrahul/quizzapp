b6950b737cc63d7f4e621473b4ace5c03f97db83
Rahul Thakur <rahul.thakur@artdexandcognoscis.com>
2021-11-29 17:39:55 +0530

    changes in model

--- quizzapp/core/models.py
+++ quizzapp/core/models.py
@@ -1,3 +1,28 @@
 from django.db import models
-
 # Create your models here.
+# class QuestionModel(models.Model):
+    
+#     img_question = models.ImageField(upload_to='Question',blank=True, null=True)
+#     question = models.TextField(null=True)
+#     option1 = models.CharField(max_length=200,null=True)
+#     option2 = models.CharField(max_length=200,null=True)
+#     option3 = models.CharField(max_length=200,null=True)
+#     option4 = models.CharField(max_length=200,null=True)
+#     correct_answer = models.CharField(max_length=200,null=True)
+#     category = models.CharField(max_length=200,null=True)
+#     sub_category = models.CharField(max_length=200,null=True)
+    
+#     def __str__(self):
+#         return self.question
+
+class QuizzHistroy(models.Model):
+    # user1,user1 score,user2,user2 score,winner,looser,created at,started at,completed at.
+    user1 = models.ForeignKey()
+    user1_score = models.IntegerField()
+    user2 = models.ForeignKey()
+    user2_score = models.IntegerField()
+    winner = models.CharField(max_length=200,null=True)
+    looser = models.CharField(max_length=200,null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    started_at = models.DateTimeField(auto_now=True,null = True)
+    completed_at = models.DateTimeField(auto_now=True,null = True)
---
+++ quizzapp/core/quizz_manager.py
@@ -0,0 +1,18 @@
+def get_random_ten_question():
+    # from the question model get 10 ques with same sub category
+    # get all question having sunbcategory similar from question models using filter. o/p: queryset
+    # select random 10 objs from the above queryset
+    # get id of all questions randomly selected in a list
+    pass
+
+def store_questions():
+    pass
+
+def validate_answer(answer):
+    pass
+
+def update_score():
+    pass
+
+def get_winner():
+    pass
